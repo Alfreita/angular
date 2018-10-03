b211911819170459e0db3121f95b383131bfa455
tc.vfreitas <tc.vfreitas@padtec.com.br>
2018-10-03 09:39:43 -0300

    adicionando arquivos pois o meu nao estava funcionando

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,25 +1,47 @@
-// public/js/controllers/foto-controller.js
-
 angular.module('alurapic')
-    .controller('FotoController', function($scope, $http) {
+	.controller('FotoController', function($scope, $http, $routeParams) {
 
-        $scope.foto = {};
-        $scope.mensagem = '';
+		$scope.foto = {};
+		$scope.mensagem = '';
 
-        $scope.submeter = function() {
+		if($routeParams.fotoId) {
+			$http.get('/v1/fotos/' + $routeParams.fotoId)
+			.success(function(foto) {
+				$scope.foto = foto;
+			})
+			.error(function(erro) {
+				console.log(erro);
+				$scope.mensagem = 'Não foi possível obter a foto'
+			});
+		}
 
-            if ($scope.formulario.$valid) {
+		$scope.submeter = function() {
 
-                $http.post('/v1/fotos', $scope.foto)
-                .success(function() {
-                    $scope.foto = {};
-                    $scope.mensagem = 'Foto cadastrada com sucesso';
-                })
-                .error(function(erro) {
-                    console.log(erro);
-                    $scope.mensagem = 'Não foi possível cadastrar a foto';
-                })
-            }
-        };
+			if ($scope.formulario.$valid) {
 
-    });
+				if($routeParams.fotoId) {
+
+					$http.put('/v1/fotos/' + $scope.foto._id, $scope.foto)
+					.success(function() {
+						$scope.mensagem = 'Foto alterada com sucesso';
+					})
+					.error(function(erro) {
+						console.log(erro);
+						$scope.mensagem = 'Não foi possível alterar';
+					});
+
+				} else { 
+
+					$http.post('/v1/fotos', $scope.foto)
+					.success(function() {
+						$scope.foto = {};
+						$scope.mensagem = 'Foto cadastrada com sucesso';
+					})
+					.error(function(erro) {
+						console.log(erro);
+						$scope.mensagem = 'Não foi possível cadastrar a foto';
+					})
+				}
+			}
+		};
+	});
--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -1,16 +1,43 @@
 angular.module('minhasDiretivas', [])
-    .directive('meuPainel', function() {
+	.directive('meuPainel', function() {
 
-        var ddo = {};
+		var ddo = {};
 
-        ddo.restrict = "AE"; //Atribute ou Element
+		ddo.restrict = "AE";
         ddo.transclude = true;
 
-        ddo.scope = {
+
+		ddo.scope = {
             titulo: '@'
         };
 
-        ddo.templateUrl = 'js/directives/meu-painel.html';           
+        ddo.templateUrl = 'js/directives/meu-painel.html';
+
+		return ddo;
+	})
+    .directive('minhaFoto', function() {
+
+        var ddo = {};
+
+        ddo.restrict = "AE";
+
+        ddo.scope = {
+            titulo: '@',
+            url: '@'
+        };
+
+        ddo.template = '<img class="img-responsive center-block" src="{{url}}" alt="{{titulo}}">';           
+        
+        return ddo;
+    })
+    .directive('meuBotaoPerigo', function() {
+        var ddo = {};
+        ddo.restrict = "E";
+        ddo.scope = {
+            nome: '@',
+            acao : '&'
+        }
+        ddo.template = '<button class="btn btn-danger btn-block" ng-click="acao()">{{nome}}</button>';
 
         return ddo;
     });
